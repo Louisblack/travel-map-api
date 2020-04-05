4a00cc22760a44e74d26dafbb023c3280f5d140d
Louis Houghton <louis@louishoughton.com>
2020-04-05 21:14:08 +0100

    Add jwt security

--- src/main/java/com/louishoughton/GreetingResource.java
+++ src/main/java/com/louishoughton/GreetingResource.java
@@ -1,10 +1,17 @@
 package com.louishoughton;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
 import java.util.Map;
 
 @Path("/hello")
@@ -12,8 +19,16 @@ import java.util.Map;
 @Consumes(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
+    @Inject
+    JsonWebToken jwt;
+
     @GET
-    public Greeting hello() {
-        return new Greeting("Hello");
+    @PermitAll
+    public Greeting hello(@Context SecurityContext ctx) {
+        Principal caller =  ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        boolean hasJWT = jwt.getClaimNames() != null;
+        String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJWT);
+        return new Greeting(helloReply);
     }
 }
